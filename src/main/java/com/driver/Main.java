eff1e88df46171f7fd0251248ffb8c164b7ab416
purshottam <pankajpatidar9521@gmail.com>
2023-09-14 07:09:50 +0530

    change made 2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
 
         public static void main(String[] args){
-            RWOnly r=new RWOnly("pankaj");
+            RWOnly r=new RWOnly();
             r.setName("pankaj");
             String n= r.getName();
             System.out.println(n);
